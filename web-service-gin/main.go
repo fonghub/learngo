5783772cafca7773e7586d5aae88728befb3e0cb
fonghub <zaofengchen@163.com>
2023-04-16 18:20:52 +0800

    ..

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -6,24 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//type album struct {
-//	ID     string  `json:"id"`
-//	Title  string  `json:"title"`
-//	Artist string  `json:"artist"`
-//	Price  float64 `json:"price"`
-//}
-//
-//var albums = []album{
-//	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-//	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-//	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-//}
-
 func main() {
 	router := gin.Default()
+	//albums
 	router.GET("/albums", internal.GetAlbums)
 	router.GET("/albums/:id", internal.GetAlbumByID)
 	router.POST("/albums/", internal.PostAlbums)
+
+	// html
+	router.LoadHTMLGlob("html/**/*")
+	router.GET("index", internal.MainIndex)
+	router.GET("/game/index", internal.GameIndex)
+	router.GET("user/index", internal.UserIndex)
+	router.GET("user/add", internal.UserAdd)
+	router.POST("user/save", internal.UserSave)
+
+	//json
+	router.GET("someJSON", internal.SomeJSON)
+	router.GET("json", internal.Json)
+	router.GET("pureJSON", internal.PureJson)
 
 	router.Run("localhost:8080")
 }
